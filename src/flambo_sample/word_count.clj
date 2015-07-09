2921238bcd7c8861478cfe4315a9db4b48055d59
Koji Yusa <odekopoon@gmail.com>
2015-07-09 23:37:55 +0900

    pprint result

--- src/flambo_sample/word_count.clj
+++ src/flambo_sample/word_count.clj
@@ -2,10 +2,11 @@
   (:require [flambo.api :as f]
             [clojure.string :as str]))
 
-(def sc  (f/local-spark-context "word-count"))
+(def sc (f/local-spark-context "word-count"))
 
 (-> (f/text-file sc "sample.txt")
     (f/flat-map (f/fn [s] (str/split s #" +")))
     (f/map (f/fn [s] [s 1]))
     (f/reduce-by-key +)
-    (f/collect))
+    (f/collect)
+    (clojure.pprint/pprint))
